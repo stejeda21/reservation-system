45197b17c1ce059a0791fb91f45f299dfc273f9a
Steven Tejeda <steventejeda@gmail.com>
2020-12-29 00:37:32 -0500

    finished version of project

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 nunjucks.configure("templates", {
-  autoescape: true,
-  express: app
+    autoescape: true,
+    express: app
 });
 
 app.use(routes);
@@ -20,19 +20,19 @@ app.use(routes);
 /** 404 handler */
 
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
-  err.status = 404;
+    const err = new Error("Not Found");
+    err.status = 404;
 
-  // pass the error to the next piece of middleware
-  return next(err);
+    // pass the error to the next piece of middleware
+    return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.render("error.html", { err });
+    return res.render("error.html", { err });
 });
 
 module.exports = app;
--- models/reservation.js
+++ models/reservation.js
@@ -4,41 +4,122 @@ const moment = require("moment");
 
 const db = require("../db");
 
-
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
-    this.id = id;
-    this.customerId = customerId;
-    this.numGuests = numGuests;
-    this.startAt = startAt;
-    this.notes = notes;
-  }
+    constructor({ id, customerId, numGuests, startAt, notes }) {
+        this.id = id;
+        this.customerId = customerId;
+        this.numGuests = numGuests;
+        this.startAt = startAt;
+        this.notes = notes;
+    }
 
-  /** formatter for startAt */
+    /** methods for getting/setting number of guests */
 
-  getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
-  }
+    set numGuests(val) {
+        if (val < 1) throw new Error("Cannot have fewer than 1 guest.");
+        this._numGuests = val;
+    }
 
-  /** given a customer id, find their reservations. */
+    get numGuests() {
+        return this._numGuests;
+    }
 
-  static async getReservationsForCustomer(customerId) {
-    const results = await db.query(
-          `SELECT id, 
+    /** methods for setting/getting startAt time */
+
+    set startAt(val) {
+        if (val instanceof Date && !isNaN(val)) this._startAt = val;
+        else throw new Error("Not a valid startAt.");
+    }
+
+    get startAt() {
+        return this._startAt;
+    }
+
+    get formattedStartAt() {
+        return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
+    }
+
+    /** methods for setting/getting notes (keep as a blank string, not NULL) */
+
+    set notes(val) {
+        this._notes = val || "";
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    /** methods for setting/getting customer ID: can only set once. */
+
+    set customerId(val) {
+        if (this._customerId && this._customerId !== val)
+            throw new Error("Cannot change customer ID");
+        this._customerId = val;
+    }
+
+    get customerId() {
+        return this._customerId;
+    }
+
+    /** given a customer id, find their reservations. */
+
+    static async getReservationsForCustomer(customerId) {
+        const results = await db.query(
+            `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
-         WHERE customer_id = $1`,
-        [customerId]
-    );
+         WHERE customer_id = $1`, [customerId]
+        );
 
-    return results.rows.map(row => new Reservation(row));
-  }
+        return results.rows.map(row => new Reservation(row));
+    }
+
+    /** find a reservation by id. */
+
+    static async get(id) {
+        const result = await db.query(
+            `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt",
+           notes
+         FROM reservations 
+         WHERE id = $1`, [id]
+        );
+
+        let reservation = results.row[0];
+
+        if (reservation === undefined) {
+            const err = new Error(`No such reservation: ${id}`);
+            err.status = 404;
+            throw err;
+        }
+
+        return new Reservation(reservation);
+    }
+
+    /** save reservation. */
+
+    async save() {
+        if (this.id === undefined) {
+            const result = await db.query(
+                `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`, [this.customerId, this.numGuests, this.startAt, this.notes]
+            );
+            this.id = result.rows[0].id;
+        } else {
+            await db.query(
+                `UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3
+             WHERE id=$4`, [this.numGuests, this.startAt, this.notes, this.id]
+            );
+        }
+    }
 }
-
 
 module.exports = Reservation;
